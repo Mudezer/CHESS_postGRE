b64206973ec30591fb2b9db94ee700612261c21b
Jboistel <jboistel@gmail.com>
2023-10-24 18:51:50 +0200

    struct chessgame & chessmove

--- src/chess.c
+++ src/chess.c
@@ -1,1 +1,20 @@
 #include <postgres.h>
+
+
+
+/************************************************************************/
+
+/* Structure to represent the chessgame */
+typedef struct
+{
+    string      pgn,
+    ChessMove[] moves;
+
+} ChessGame;
+
+typedef struct
+{
+    string      move,
+    int        moveNumber;
+} ChessMove;
+
