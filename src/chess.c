bf2767209fbbf7a9533e4ede286857de24fa35ae
Mudezer <loic.bermudez@gmail.com>
2023-10-25 16:46:29 +0200

    chessboard structure created + smallchesslib added

---
+++ src/chess.c
@@ -0,0 +1,24 @@
+#include "postgres.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+#define BOARD_SIZE 8
+#define BOARD_SIZE_SQUARE 64
+#define MAX_FEN_LENGTH 100
+
+typedef struct{
+
+    char fen[MAX_FEN_LENGTH]; //
+    char board[BOARD_SIZE][BOARD_SIZE]; // état de la table
+    char turn; // à qui de jouer
+    char castling[4]; // roque
+    char en_passant[2]; // en passant
+    char halfmove_clock; // halfmove clock
+    char fullmove_number; // each 2 halfmoves clock
+
+} postgres_chessboard;
+
+
---
+++ src/test.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS chess_table;
+DROP EXTENSION IF EXISTS chess;
+
